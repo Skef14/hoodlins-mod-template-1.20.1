0d333ce03fd820d704564833710ac643209e0047
Denis Skef <djdjdndnsj56@gmail.com>
2023-07-10 00:20:34 +0300

    Initial commit

---
+++ src/main/java/net/skef/hoodlinsmod/HoodlinsModClient.java
@@ -0,0 +1,10 @@
+package net.skef.hoodlinsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HoodlinsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
